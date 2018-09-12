4739236f5250206d51e1d6c94648c38674c786b9
= <sholyboy89@gmail.com>
2018-09-12 17:44:23 -0500

    parse location and longitude from exiftags

--- stitch.cpp
+++ stitch.cpp
@@ -47,6 +47,7 @@ Location getCoodinates(string path) {
     	error += ": No Exif data found in the file";
     	throw Exiv2::Error(1, error);
     }
+
     Exiv2::ExifData::const_iterator end = exifData.end();
     Exiv2::Value::AutoPtr latV = Exiv2::Value::create(Exiv2::signedRational);
     Exiv2::Value::AutoPtr longV = Exiv2::Value::create(Exiv2::signedRational);
@@ -55,28 +56,16 @@ Location getCoodinates(string path) {
     auto latPos = exifData.findKey(latitudeKey);
     auto longPos = exifData.findKey(longitudeKey);
     if (latPos == exifData.end() || longPos == exifData.end()) throw Exiv2::Error(1, "Key not found");
+
     // Get a pointer to a copy of the value
     latV = latPos->getValue();
     longV = longPos->getValue();
     auto latitude = latV->toFloat() + (latV->toFloat(1) / 60.0) + (latV->toFloat(2) / 3600.0); 
     auto longitude =longV->toFloat() + (longV->toFloat(1) / 60.0) + (longV->toFloat(2) / 3600.0);
-										
-    cout << "Latitude of this image is "<<latitude<<endl;
-    cout << "Longitude of this image is "<<longitude<<endl;
-    for (Exiv2::ExifData::const_iterator i = exifData.begin(); i != end; ++i) {
-        const char* tn = i->typeName();
-        std::cout << std::setw(44) << std::setfill(' ') << std::left
-                  << i->key() << " "
-                  << "0x" << std::setw(4) << std::setfill('0') << std::right
-                  << std::hex << i->tag() << " "
-                  << std::setw(9) << std::setfill(' ') << std::left
-                  << (tn ? tn : "Unknown") << " "
-                  << std::dec << std::setw(3)
-                  << std::setfill(' ') << std::right
-                  << i->count() << "  "
-                  << std::dec << i->value()
-                  << "\n";
-    }
+
+    loc.longitude = longitude;
+    loc.latitude = latitude;
+    
     return loc;
 }
 
