d186437b8a4e09b168b6e701bfd1f7e6e22a2e42
= <sholyboy89@gmail.com>
2018-09-26 14:59:27 -0500

    add distance to for position objects

--- image.hpp
+++ image.hpp
@@ -2,10 +2,31 @@
 #define SHOIMAGE_HPP_
 
 #include <string>
+#include <math.h>
+
+float toRadian (float deg) {
+	return deg * M_PI/180;
+}
 
 struct Location {
 	float longitude;
 	float latitude;
+
+	float distanceTo(Location loc) {
+		auto earthRadius = 6371e3;
+		auto longRad = toRadian(this.longitude);
+		auto latRad = toRadian(this.latitude);
+
+		auto locLongRad = toRadian(loc.longitude);
+		auto locLatRad = toRadian(loc.latitude);
+
+		auto dLat = locLatRad - latRad;
+		auto dLong = locLongRad - longRad;
+
+		auto  a = sin(dLat/2) * sin(dLat/2) + cos(longRad) * cos(locLongRad) * sin(dLong/2) * sin(dLong/2);
+		auto c =2 * atan(sqrt(a), sqrt(1-a));
+		return earthRadius * c;
+	}
 };
 
 
