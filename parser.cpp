21db9ac67ec7e98f63dba39fb0f0db3cd9750e50
= <sholyboy89@gmail.com>
2018-10-02 17:31:51 -0500

    add csv parser and csv reader

--- parser.cpp
+++ parser.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <utility>
+#include <map>
 #include <array>
+#include <algorithm>
 #include <vector>
+
+#include "csv.h"
 
 using std::cout;
 using std::string;
 using std::ifstream;
 using std::ofstream;
 using std::vector;
+using std::pair;
+using std::find;
 using std::endl;
+using std::map;
+
+struct Matchdetails {
+	double matchPercentage;
+	int numMatches;
+};
 
 int main (int argc, char* argv[]) {
 	if (argc < 3) {
-		cout << "Program usage: <results file> <distances file>"<<endl;
+		cout << "Program usage: <Keypoints file> <Distances file>"<<endl;
 		exit(1);
 	}
 
@@ -24,5 +37,32 @@ int main (int argc, char* argv[]) {
 	string h;
 	getline(infile, h);
 
+	int numMatches;
+	string queryImageName, matchImageName;
+	double matchKeypointPercentage, distance;
+	map <pair<string, string>, Matchdetails> imagePairs;
+
+	while(infile >> queryImageName >> matchImageName >> numMatches >> matchKeypointPercentage) {
+		cout << "Making a read"<<endl;
+		auto aPair = make_pair(queryImageName, matchImageName);
+		Matchdetails detail{matchKeypointPercentage, numMatches};
+		imagePairs[aPair] = detail;
+
+	}
+	infile.close();
+
+	//Finished parsing keypoints file, parse distances file.
+	infile.open(argv[2]);
+	getline(infile, h);
+
+	while (infile >> queryImageName >> matchImageName >> distance) {
+		auto aPair = make_pair(queryImageName, matchImageName);
+		if (imagePairs.find(aPair) != imagePairs.end()) {
+			outfile << queryImageName << ","<<matchImageName << "," << imagePairs[aPair].numMatches << ","
+			<<imagePairs[aPair].matchPercentage<< ","<<distance<<","<<endl;
+		}
+	}
+	infile.close(); outfile.close();
+	
 	return 0;
 }
