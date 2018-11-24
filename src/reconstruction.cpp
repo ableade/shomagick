a8850b792a96440d8753e86bf7eaa8fe8dc3303e
Adebodun Adekunle <sola@kosherkampus.com>
2018-11-23 21:33:08 -0600

    add src directory:

--- src/reconstruction.cpp
+++ src/reconstruction.cpp
@@ -1,27 +1,49 @@
 #include "reconstruction.h"
 #include <vector>
 
-using std::vector;
 using cv::Mat;
 using cv::Mat_;
+using cv::Point2f;
+using cv::countNonZero;
+using std::vector;
 
-Reconstructor :: Reconstructor (FlightSession flight, TrackGraph tg): flight (flight), tg(tg)  {};
+Reconstructor ::Reconstructor(FlightSession flight, TrackGraph tg) : flight(flight), tg(tg){};
 
-void Reconstructor::computeEssentialMatrix(string image1,  string image2,  Camera camera, int method, double tresh, double prob) {
-    Mat cameraMatrix = (Mat_<double>(3,3) << 3.8123526712521689e+3,0.2592, 0 ,3.8123526712521689e+03,1944,0, 0.1);
+void Reconstructor::_alignMatchingPoints(string image1, string image2, std::vector<cv::Point2f> &points1, std::vector<cv::Point2f> &points2)
+{
+    Mat cameraMatrix = (Mat_<double>(3, 3) << 3.8123526712521689e+3, 0.2592, 0, 3.8123526712521689e+03, 1944, 0, 0.1);
     auto allPairMatches = this->flight.loadMatches(image1);
     auto pairwiseMatches = allPairMatches[image2];
     auto features2 = this->flight.loadFeatures(image2);
     auto features1 = this->flight.loadFeatures(image1);
 
-    vector <cv::Point2f> impoints1, impoints2;
+    vector<cv::Point2f> impoints1, impoints2;
     //align pairwise matches
-    for(auto dmatch : pairwiseMatches) {
+    for (auto dmatch : pairwiseMatches)
+    {
         auto point1 = features1.first[dmatch.queryIdx].pt;
         auto point2 = features2.first[dmatch.trainIdx].pt;
-        impoints1.push_back(point1);
-        impoints2.push_back(point2);
+        points1.push_back(point1);
+        points2.push_back(point2);
     }
+}
+void Reconstructor::recoverTwoCameraViewPose(string image1, string image2, int method, double tresh, double prob)
+{
+    vector<Point2f> points1;
+    vector <Point2f> points2;
+    this->_alignMatchingPoints(image1, image2, points1, points2);
+    Mat cameraMatrix = (Mat_<double>(3, 3) << 3.8123526712521689e+3, 0.2592, 0, 3.8123526712521689e+03, 1944, 0, 0.1);
     Mat mask;
-    Mat essentialMatrix = cv::findEssentialMat(impoints1, impoints2, cameraMatrix, method, tresh, prob, mask);
+    Mat essentialMatrix = cv::findEssentialMat(points1, points2, cameraMatrix, method, tresh, prob, mask);
+}
+
+float Reconstructor::computeReconstructability (int commonPoints, Mat mask, int tresh) {
+    auto inliers = countNonZero(mask);
+    auto outliers = commonPoints - inliers;
+    auto ratio = float(outliers) / commonPoints;
+    return ratio > tresh ? ratio :  0;
+}
+
+void Reconstructor::computePlaneHomography(string image1, string image2) {
+
 }
--- src/shotracking.h
+++ src/shotracking.h
@@ -12,33 +12,33 @@ struct VertexProperty
 	bool is_image;
 };
 
-typedef boost::adjacency_list<boost::listS, boost::setS, boost::undirectedS, VertexProperty, boost::no_property> TrackGraph; 
-typedef std::pair <string, int > FeatureNode;
+typedef boost::adjacency_list<boost::listS, boost::setS, boost::undirectedS, VertexProperty, boost::no_property> TrackGraph;
+typedef std::pair<string, int> FeatureNode;
 
 class ShoTracker
 {
-private:
+  private:
 	FlightSession flight;
 	std::map<string, std::vector<string>> candidateImages;
-	std::map <std::pair <string, int>, int> features;
-	std::map <int, std::vector <int>> tracks;
+	std::map<std::pair<string, int>, int> features;
+	std::map<int, std::vector<int>> tracks;
 	UnionFind uf;
-	int minTrackLength =2;
-	bool addFeatureToIndex(std::pair <string, int> feature, int featureIndex);
-	std::map <string, TrackGraph::vertex_descriptor> imageNodes;
-	std::map <string, TrackGraph::vertex_descriptor> trackNodes;
-	std::set<std::pair < string , string> > getCombinations_ (std::vector< string> images);
-	
-public:
+	int minTrackLength = 2;
+	bool addFeatureToIndex(std::pair<string, int> feature, int featureIndex);
+	std::map<string, TrackGraph::vertex_descriptor> imageNodes;
+	std::map<string, TrackGraph::vertex_descriptor> trackNodes;
+	std::set<std::pair<string, string>> getCombinations_(std::vector<string> images);
+
+  public:
 	ShoTracker(FlightSession flight, std::map<string, std::vector<string>> candidateImages);
-	void createTracks(std::vector < std::pair <FeatureNode, FeatureNode>> features);
+	void createTracks(std::vector<std::pair<FeatureNode, FeatureNode>> features);
 	TrackGraph buildTracksGraph();
 	void mergeFeatureTracks(FeatureNode feature1, FeatureNode feature2);
-	std::vector< std::pair <FeatureNode, FeatureNode>> createFeatureNodes();
-	std::map<std::pair < string, string>, std::vector<string> > commonTracks(const TrackGraph& tg);
-	std::map <int, std::vector <int>> getTracks();
+	std::vector<std::pair<FeatureNode, FeatureNode>> createFeatureNodes();
+	std::map<std::pair<string, string>, std::vector<string>> commonTracks(const TrackGraph &tg);
+	std::map<int, std::vector<int>> getTracks();
 	FeatureNode retrieveFeatureByIndexValue(int index);
-	std::map <string, TrackGraph::vertex_descriptor> getTrackNodes();
-	std::map <string, TrackGraph::vertex_descriptor> getImageNodes();
+	std::map<string, TrackGraph::vertex_descriptor> getTrackNodes();
+	std::map<string, TrackGraph::vertex_descriptor> getImageNodes();
 };
 #endif
