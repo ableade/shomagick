85cd66ad7d27b4ae4ea0d16e7cebb563b3db37dd
Adebodun Adekunle <sola@kosherkampus.com>
2019-09-21 06:23:14 -0800

    save

---
+++ src/cudasurf.cpp
@@ -0,0 +1,49 @@
+#include "cudasurf.hpp"
+
+using std::vector;
+using cv::InputArray;
+using cv::OutputArray;
+using cv::KeyPoint;
+
+CudaSurfFeatureDetector::CudaSurfFeatureDetector(
+        double hessianThreshold, 
+        int nOctaves,
+        int nOctaveLayers, 
+        bool extended, 
+        float keyPointsRatio, 
+        bool upright
+    ):cudaSurf_(
+        hessianThreshold, 
+        nOctaves,
+        nOctaveLayers, 
+        extended, 
+        keyPointsRatio, 
+        upright
+    ) {}
+
+ cv::Ptr<CudaSurfFeatureDetector> CudaSurfFeatureDetector::create(
+     double hessianThreshold, 
+     int nOctaves,
+     int nOctaveLayers,
+     bool extended, 
+     float keyPointsRatio,
+     bool upright
+ ) 
+ {
+     return cv::makePtr<CudaSurfFeatureDetector>(hessianThreshold, nOctaves, nOctaveLayers, extended, keyPointsRatio, upright);
+ }
+
+ void CudaSurfFeatureDetector::detect(InputArray image, vector<KeyPoint>& keypoints, InputArray mask) {
+     cudaSurf_(image.getGpuMat(), mask.getGpuMat(), keypoints);
+ }
+ 
+ void CudaSurfFeatureDetector::detectAndCompute(
+     InputArray image, 
+     cv::InputArray mask, 
+     vector<KeyPoint>& keypoints, 
+     OutputArray &descriptors,
+     bool useProvidedKeypoints
+){
+    cudaSurf_(image.getGpuMat(), mask.getGpuMat(), keypoints, descriptors.getGpuMat(), useProvidedKeypoints);
+}
+
