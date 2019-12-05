bd9624634aaa08fad0ddf2d11d5ed343f98ac1ce
Sola Adekunle <sholyboy89@gmail.com>
2019-12-04 19:42:30 -0600

    New kd tree with n neighbours spatial search

--- include/shomatcher.hpp
+++ include/shomatcher.hpp
@@ -6,8 +6,13 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include "point.h"
+#include "kdtree.h"
 
 const int FEATURE_PROCESS_SIZE = 2500;
+const int KD_TREE_DIMENSION_SIZE = 2;
+
+using KDTreeDataSet = std::vector<std::pair<Point<KD_TREE_DIMENSION_SIZE>, std::string>>;
 
 inline double dist_sq(double *a1, double *a2, int dims)
 {
@@ -24,12 +29,13 @@ class ShoMatcher
 {
 private:
     FlightSession flight_;
-    void *kd_;
-    int dimensions_ = 2;
     int featureSize_;
-    std::map<std::string, std::vector<std::string>> candidateImages;
+    std::map<std::string, std::vector<std::string> > candidateImages;
     bool _extractFeature(std::string fileName);
     cv::Ptr<RobustMatcher> rMatcher_;
+    KDTree<KD_TREE_DIMENSION_SIZE, std::string> kd_;
+    KDTreeDataSet kdTreeData_;
+    void buildKdTree_();
 
 public:
     ShoMatcher(
@@ -37,11 +43,10 @@ public:
         int featureSize = FEATURE_PROCESS_SIZE, 
         RobustMatcher::Feature featureType = RobustMatcher::Feature::orb
     );
-    void getCandidateMatchesUsingSpatialSearch(double range = 0.000125);
+    void getCandidateMatchesUsingKNNSearch(int n);
     void getCandidateMatchesFromFile(std::string candidateFile);
     int extractFeatures();
     void runRobustFeatureMatching();
-    void buildKdTree();
     std::map<std::string, std::vector<std::string>> getCandidateImages() const;
     void plotMatches(std::string img1, std::string img2) const;
 };
